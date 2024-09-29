0a1269e662d5b771363d2d12005932a5697bcc3c
shoto87 <iampratik70@gmail.com>
2024-09-30 00:56:46 +0530

    added portfolio

---
+++ portfolio/js/index.js
@@ -0,0 +1,17 @@
+let dayNight = document.querySelector(".dayNight");
+let banner = document.querySelector(".banner");
+
+dayNight.addEventListener("click", () => {
+  banner.classList.toggle("night");
+});
+
+let typing = new Typed("#text", {
+  strings: ["patrick", "a Designer !", "an Artist !", "a Developer !"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+  showCursor: true,
+  backDelay: 1000,
+
+  cursorChar: "|",
+});
